abc7bb5d76a87c87a5dc2a62102cc38c3a78d216
Rosux <jcsbt2015@gmail.com>
2022-12-25 03:03:34 +0100

    V2

--- DarkMode.js
+++ DarkMode.js
@@ -1,156 +1,130 @@
-// =============================String to bool============================= //
-String.prototype.toBool = function(){return /^true$/i.test(this);};
-// =============================Darkmode Class============================= //
-class Darkmode{
-    constructor(darkModeTheme, lightModeTheme, darkByDefault=true){
-        this.darkModeTheme = darkModeTheme;
-        this.lightModeTheme = lightModeTheme;
-        if(this.getCookie("Rosux-Darkmode")){
-            this.dark = this.getCookie("Rosux-Darkmode").toBool();
-        }else{
-            darkByDefault ? this.dark = true : this.dark = false;
-            this.updateCookie();
-        }
-        this.updateDocument();
-        this.addButtonListeners();
+class DarkMode{
+    /**
+     * @param {string} darkModeTheme A string of css variables for a dark theme
+     * @param {string} lightModeTheme A string of css variables for a light theme
+     */
+    constructor(darkModeTheme="", lightModeTheme=""){
+        this.dmt = darkModeTheme;
+        this.lmt = lightModeTheme;
+        this.mode = this.#getState();
+        this.setTheme();
+        if(document.readyState==="complete"||document.readyState === "interactive"){this.applyListener();}else{window.addEventListener("DOMContentLoaded",()=>{this.applyListener();});}
+        this.updateButtons();
     }
-    switch(){
-        this.dark = !this.dark;
-        this.updateCookie();
-        this.updateDocument();
+    /**
+     * Set the theme to the users prefered color scheme
+     */
+    defaultColorScheme(){
+        this.mode = (window.matchMedia('(prefers-color-scheme: dark)').matches);
+        this.#setUpdateSave();
     }
+    /**
+     * Set the theme to dark
+     */
     on(){
-        this.dark = true;
-        this.updateCookie();
-        this.updateDocument();
+        this.mode = true;
+        this.#setUpdateSave();
     }
+    /**
+     * Set the theme to light
+     */
     off(){
-        this.dark = false;
-        this.updateCookie();
-        this.updateDocument();
+        this.mode = false;
+        this.#setUpdateSave();
     }
-    updateCookie(){
-        if(!this.getCookie("Rosux-Darkmode")){
-            this.setCookie("Rosux-Darkmode", this.dark, 30);
-        }
-        this.setCookie("Rosux-Darkmode", this.dark, 30);
+    /**
+     * Toggle between the themes
+     */
+    toggle(){
+        this.mode = !this.mode;
+        this.#setUpdateSave();
     }
+    /**
+     * Returns Boolean (true if dark || false if light)
+     */
     getMode(){
-        return this.dark;
+        return this.mode;
     }
-    async addButtonListeners(){
-        if(document.readyState === "complete" || document.readyState === "interactive"){
-            const buttons = document.querySelectorAll("*[darkmode-selector]");
-            applyListener(buttons);
-        }else{
-            window.addEventListener("DOMContentLoaded", ()=>{
-                const buttons = document.querySelectorAll("*[darkmode-selector]");
-                applyListener(buttons);
+    /**
+     * Add eventListeners to elements with "darkmode-switch" & "darkmode-selector" attributes
+     */
+    async applyListener(){
+        // on off etc
+        const modeButton = document.querySelectorAll("[darkmode-button]");
+        for(let i=0;i<modeButton.length;i++){
+            modeButton[i].addEventListener("click", (e)=>{
+                if(e.target.getAttribute("darkmode-button") === "on"){
+                    this.on();
+                }else if(e.target.getAttribute("darkmode-button") === "off"){
+                    this.off();
+                }else if(e.target.getAttribute("darkmode-button") === "switch"){
+                    this.toggle();
+                }else if(e.target.getAttribute("darkmode-button") === "default"){
+                    this.defaultColorScheme();
+                }
+                // add some attribute for css styling
+                e.target.setAttribute("darkmode-data", this.mode);
             });
         }
-        function applyListener(buttons){
-            for(let i=0;i<buttons.length;i++){
-                buttons[i].addEventListener('change', (e)=>{
-                    if(e.target.getAttribute("darkmode-selector") == "1"){
-                        e.target.checked ? darkmode.off() : darkmode.on();
-                    }else if(buttons[i].getAttribute("darkmode-selector") == "2"){
-                        e.target.checked ? darkmode.on() : darkmode.off();
-                    }
-                });
-            }
-        }
-    }
-    updateDocument(){
-        if(document.readyState === "complete" || document.readyState === "interactive"){
-            this.dark ? document.body.style.cssText = this.darkModeTheme : document.body.style.cssText = this.lightModeTheme;
-            this.updateButtons();
-        }else{
-            window.addEventListener("DOMContentLoaded", ()=>{
-                this.dark ? document.body.style.cssText = this.darkModeTheme : document.body.style.cssText = this.lightModeTheme;
-                this.updateButtons();
+        // checkboxes
+        const buttonCheckbox = document.querySelectorAll("[darkmode-selector]");
+        for(let i=0;i<buttonCheckbox.length;i++){
+            buttonCheckbox[i].addEventListener('change', (e)=>{
+                if(e.target.getAttribute("darkmode-selector") === "false"){
+                    e.target.checked ? this.off() : this.on();
+                }else{
+                    e.target.checked ? this.on() : this.off();
+                }
             });
         }
+        this.updateButtons();
     }
+    /**
+     * Update the switch and attributes for elements with "darkmode-switch" & "darkmode-selector" attributes
+     */
     async updateButtons(){
-        let x = this.dark;
-        const buttons = document.querySelectorAll("*[darkmode-selector]");
-        for(let i=0;i<buttons.length;i++){
-            if(x){
-                // set buttons do dark
-                if(buttons[i].getAttribute("darkmode-selector") == "1" || buttons[i].getAttribute("darkmode-selector") == "s"){
-                    buttons[i].checked = false;
-                }else if(buttons[i].getAttribute("darkmode-selector") == "2"){
-                    buttons[i].checked = true;
-                }
+        // set switch to right pos
+        const modeButton = document.querySelectorAll("[darkmode-button]");
+        for(let i=0;i<modeButton.length;i++){
+            modeButton[i].setAttribute("darkmode-data", this.mode);
+        }
+        // set checkbox to right pos
+        const buttonCheckbox = document.querySelectorAll("[darkmode-selector]");
+        for(let i=0;i<buttonCheckbox.length;i++){
+            const button = buttonCheckbox[i];
+            if(button.getAttribute("darkmode-selector") === "false"){
+                this.mode ? button.checked = false : button.checked = true;
             }else{
-                // set buttons do light
-                if(buttons[i].getAttribute("darkmode-selector") == "1" || buttons[i].getAttribute("darkmode-selector") == "s"){
-                    buttons[i].checked = true;
-                }else if(buttons[i].getAttribute("darkmode-selector") == "2"){
-                    buttons[i].checked = false;
-                }
+                this.mode ? button.checked = true : button.checked = false;
             }
         }
     }
-    setCookie(name, val, time=30){
-        var date=new Date;
-        date.setDate(date.getDate()+time);
-        document.cookie = name + "=" + encodeURIComponent(val) + ";expires=" + date + "; path=/; sameSite=lax;";
+    /**
+     * Update the DOM css variables with given themes
+     */
+    setTheme(){
+        this.mode ? document.body.style.cssText = this.dmt : document.body.style.cssText = this.lmt;
     }
-    getCookie(name){
-        const value = `; ${document.cookie}`;
-        const parts = value.split(`; ${name}=`);
-        if (parts.length === 2) return parts.pop().split(';').shift();
+    /**
+     * shortcut for updating dom, updating buttons, saving to localstorage
+     */
+    #setUpdateSave(){
+        this.setTheme();
+        this.updateButtons();
+        this.#saveState();
+    }
+    /**
+     * get the theme from localstorage. If it does not exist get the device color scheme
+     */
+    #getState(){
+        // if state doesnt exist default to the defaultStyle
+        localStorage.getItem("darkmode") === null && localStorage.setItem('darkmode', (window.matchMedia('(prefers-color-scheme: dark)').matches));
+        return (localStorage.getItem('darkmode') === "true");
+    }
+    /**
+     * save the theme mode to localstorage
+     */
+    #saveState(){
+        localStorage.setItem('darkmode', this.mode);
     }
 }
-/*
-
-
-// ===========================Setting Up Darkmode=========================== //
-Example:
-    let darkModeTheme = "--header-color:hsl(0,0%,20%);--text-color:#d3d3d3;--background-color:hsl(0,0%,9%);--accent-color-1:#202020;--accent-color-2:#303030;";
-    let lightModeTheme = "--header-color:hsl(0,0%,80%);--text-color:hsl(0,0%,17%);--background-color:hsl(0,0%,91%);--accent-color-1:hsl(0,0%,87%);--accent-color-2:hsl(0,0%,81%);";
-    let darkmode = new Darkmode(darkModeTheme, lightModeTheme, true);
-
-Details:
-    let darkmode = new DarkMode(
-        String : darkModeTheme
-        String : lightModeTheme
-        Bool   : darkByDefault? (default = true)
-    )
-
-Notes:
-    the darkmodetheme and lightmode theme are currently implemented to be a string of :root variables
-    so to use this you would need to create your site with colors/styles based on :root variables
-
-
-
-
-
-
-///////////////////////////////////////////////////////////////////
-///////////////simple explanation for custom buttons///////////////
-///////////////////////////////////////////////////////////////////
-
-darkmode-selector="1"  =   if set true will make it light
-darkmode-selector="2"  =   if set true will make it dark
-
-///////////////////////////////////////////////////////////////////
-/////////////////////////explanation setup/////////////////////////
-///////////////////////////////////////////////////////////////////
-ex1:
-    button example [dark, bright]:
-    <div class="button-switch">
-        <input type="checkbox" id="darkmode" darkmode-selector="1">
-        <label for="darkmode"><p>Dark</p><p>Bright</p></label>
-    </div>
-ex2:
-    button example [Bright, dark]:
-    <div class="button-switch">
-        <input type="checkbox" id="darkmode" darkmode-selector="2">
-        <label for="darkmode"><p>Bright</p><p>Dark</p></label>
-    </div>
-
-
-
-*/
--- README.md
+++ README.md
@@ -1,7 +1,88 @@
+[![License](https://img.shields.io/badge/License-MIT-blue.svg)](LICENSE)
 # DarkMode
-Library to implement a simple darkmode feature
+## [Example](https://rosux.github.io/DarkMode/)
 
-For help/documentation go to the github pages or make a merge request (i think thats what its called kinda new to this)
+A simple JavaScript class for enabling dark mode on a website.
 
+Made by Rosux.
 
-<a href="https://rosux.github.io/DarkMode/example.html">Github Pages</a>
+## Installation
+
+To use this class on your website, include the `DarkMode.js` file in your project and instantiate a new DarkMode object with the required parameters.
+
+```html
+<script src="DarkMode.js"></script> <!-- no defer needed -->
+<script>
+    const dark = "--background-color: black;";
+    const light = "--background-color: light;";
+    const darkMode = new DarkMode(dark, light);
+</script>
+```
+## Usage
+
+To toggle dark mode on and off, simply call the toggle() method on the DarkMode object.
+
+```js
+darkMode.toggle();
+```
+
+You can also set dark mode to be on or off by calling the on() or off() methods, respectively.
+
+```js
+darkMode.on(); // sets dark mode to on
+darkMode.off(); // sets dark mode to off
+```
+You can set the theme to the user's preferred color scheme by calling the defaultColorScheme() method.
+
+```js
+darkMode.defaultColorScheme();
+```
+You can check the current state of dark mode by accessing the getMode() method, which returns a Boolean (true if dark, false if light).
+
+```js
+if (darkMode.getMode()) {
+  console.log('Dark mode is on');
+} else {
+  console.log('Dark mode is off');
+}
+```
+
+Customization
+
+You can customize the colors used in dark mode by passing an options object to the DarkMode constructor.
+
+```js
+const darkMode = new DarkMode({
+  darkModeTheme: 'css variables for dark theme',
+  lightModeTheme: 'css variables for light theme'
+});
+```
+You can also add event listeners to elements with darkmode-button and darkmode-selector attributes to control the dark mode state.
+
+```html
+<button darkmode-button="switch">switch</button>
+<button darkmode-button="on">make it dark</button>
+<button darkmode-button="off">make it light</button>
+<button darkmode-button="default">default to prefered color scheme</button>
+<input type="checkbox" darkmode-selector="true" value='if its checked its dark (defaults to true)'>
+<input type="checkbox" darkmode-selector="false" value='if its checked its light (defaults to true)'>
+```
+
+You can add eventListeners to new elements with darkmode-button and darkmode-selector attributes. This also automatically updates the buttons state.
+```js
+darkMode.applyListener();
+```
+
+# Contributing
+
+We welcome contributions to this project! If you have an idea for a new feature or a bug to report, please open an issue. If you want to submit a fix or a new feature, please create a pull request.
+
+Before submitting a pull request, please make sure to:
+
+    Write tests for any new code (simple examples on a page to prove it works)
+    Follow the existing code style
+    Document any changes in a log file
+
+Thank you for considering contributing to DarkMode!
+
+Let's make dark the new standard.
